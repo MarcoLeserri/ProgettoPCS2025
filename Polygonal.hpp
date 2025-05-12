5d89252729c5d14522148b3ae7aa46dc6ae01549
Cristiano Dutto <ila.dutto@gmail.com>
2025-05-12 19:12:01 +0200

    aggiornamento Cell

---
+++ Polygonal.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include "Eigen/Eigen"
+
+using namespace std;
+using namespace Eigen;
+
+namespace PolygonalLibrary {
+
+struct Polygonal
+{	
+	unsigned int NumCell0Ds = 0; // num punti
+	unsigned int NumCell1Ds = 0; // num segmenti
+	unsigned int NumCell2Ds = 0; // num poligoni 
+	
+	vector<unsigned int> Cell0DsID = {}; //registra ID punti
+	vector<unsigned int> Cell1DsID = {};
+	vector<unsigned int> Cell2DsID = {};
+	
+	Eigen::MatrixXd Cell0DsCoordinates = {}; 
+	Eigen::MatrixXi Cell1DsExtrema = {};
+	vector<array<unsigned int,3>> Cell2DsVertices = {};
+	vector<array<unsigned int,3>> Cell2DsEdges = {};
+	
+};
---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "Eigen/Eigen"
+#include "PolygonalMesh.hpp"
+#include "Utils.hpp"
+#include "UCDUtilities.hpp"
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+using namespace Eigen;
+using namespace PolygonalLibrary;
+
+
+int main(int argc, char *argv[])
+{	
+	double err = 1.0e-16
+	if (argc < 5)
+	{
+		cerr << "Error: insert at least four values" << endl; 
+		return 1; 
+	}
+	
+	int p = stoi(argv[1]);
+	int q = stoi(argv[2]);
+	int b = stoi(argv[3]);
+	int c = stoi(argv[4]);
+	
+	if (abs(p-3) < err and  q >= 3)
+	{
+		if ((abs(b) < err and  c >= 1) or  (abs(c) < err and  b >= 1)) // Class I
+		{
+		// 
+		}
+		elseif( abs(b-c) < err) {
+		// polyhedrons of II Class 
+		}
+		else {
+			cerr << "Error: polyhedron belongs to III class " << endl;			
+		}	
+	}
+	else {
+		cerr << "Error: values out of range" << endl;
+	}
+		
+	
+
+
+
+	Polygonal mesh
