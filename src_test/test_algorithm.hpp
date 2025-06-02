5e632e26b333d43bf069db6d07400ac47cdefa8a
Cristiano Dutto <ila.dutto@gmail.com>
2025-06-02 09:49:08 +0200

    test

--- src_test/test_algorithm.hpp
+++ src_test/test_algorithm.hpp
@@ -77,11 +77,39 @@ namespace PathLibrary {
 
 TEST(TestPath, TestShortestPath)
 {
-    int id = 5;
-    int idOrd = 1;
-	vector<array<unsigned int, 3>> FacceAd = {{1,2,5}, {3,4,5}, {2,3,5}, {4,1,5}};
-	vector<array<unsigned int, 3>> SortedFaces = {{1,2,5}, {2,3,5}, {3,4,5}, {4,1,5}};
-    EXPECT_EQ(SortFaces(id, idOrd, FacceAd), SortedFaces);
+   int id1 = 1;
+   int id2 = 2;
+   PolygonalDual mesh;
+   mesh.NumCell0Ds = 10;
+   mesh.NumCell1Ds = 24;
+   mesh.NumCell2Ds = 16;
+
+   mesh.Cell0DsID = {0,1,2,3,4,5,6,7,8,9};
+   mesh.Cell1DsID = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
+   mesh.Cell2DsID = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+   
+ 
+   mesh.Cell0DsCoordinates = Eigen::MatrixXd::Zero(3, mesh.NumCell0Ds);
+   mesh.Cell1DsExtrema = Eigen::MatrixXi::Zero(2, mesh.NumCell1Ds);
+   
+   mesh.Cell0DsCoordinates << 1.0, 1.0, -1.0, -1.0, 1.0, 0.0, 0.0, 0.0, -1.0, 0.0, 
+							1.0, -1.0, 1.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0, -1.0,
+							1.0, -1.0, -1.0, 1.0, 0.0, 0.0, -1.0, 1.0, 0.0, 0.0;
+
+   mesh.Cell1DsExtrema << 1, 4, 0, 5, 2, 6, 4, 4, 5, 5, 8, 7, 0, 7, 3, 8, 1, 9, 6, 9, 8, 4, 9, 7,
+							4, 0, 5, 2, 6, 1, 6, 5, 6, 8, 7, 5, 7, 3, 8, 2, 9, 3, 9, 8, 6, 9, 7,4;
+   
+   mesh.Cell2DsVertices = {{1,6,4}, {6,2,5}, {4,6,5}, {0,4,5}, {2,8,5}, {7,8,3}, {5,8,7}, {0,5,7}, {1,6,9}, {6,2,8}, {9,6,8}, {9,8,3},
+	   {4,1,9}, {7,9,3}, {4,9,7}, {0,4,7}   
+   };
+   
+   mesh.Cell2DsEdges = {{0,5,6}, {8,4,3}, {6,8,7}, {1,7,2}, {3,15,9}, {10,14,13}, {9,10,11}, {2,11,12}, {16,5,18}, {20,4,15}, {18,20,19}, {17,19,14},
+	   {0,16,21}, {22,17,13}, {21,22,23}, {1,23,12}
+   };
+
+   vector<unsigned int> Path = {1,6,2}; 
+   EXPECT_EQ(ShortestPath(id1, id2, mesh), Path);
 }
 
 }
+
